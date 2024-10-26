37484413fd9a16023546faafd9138a2d6c2d95cd
Rufree <tksk2884@gmail.com>
2024-10-26 15:56:12 +0900

    auth/Status, 미들웨어 추가

---
+++ src/service/middleware.ts
@@ -0,0 +1,32 @@
+import { connectPool } from "./db";
+import { ERROR_DB_INVALID } from "../utils/errorMessage";
+import { getUserInfo } from "./userService";
+
+export default async function middleware(req: any, res: any, next: () => void) {
+    if (connectPool == null) {
+        return res.status(500).json({
+            errorCode: ERROR_DB_INVALID,
+            error: "DB connection failed",
+        });
+    }
+
+    let accessToken: string =
+        (req.query.accessToken as string) ?? req.body.accessToken ?? "";
+
+    // const accessToken = req.cookies.accessToken;
+
+    if (accessToken != "") {
+        let result = await getUserInfo(accessToken);
+
+        if (result == null) {
+            return res.status(401).json({
+                errorCode: ERROR_DB_INVALID,
+                error: "Access Token is wrong",
+            });
+        }
+
+        res.locals.account = result;
+    }
+
+    next();
+}
