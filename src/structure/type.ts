985f4a07c5859125b8f56331f868745a7a8ee10b
Rufree <tksk2884@gmail.com>
2024-10-30 16:01:26 +0900

    Board관련 타입 추가

--- src/structure/type.ts
+++ src/structure/type.ts
@@ -2,3 +2,23 @@ export interface UserInfo {
     id: string;
     nickname: string;
 }
+
+export interface BoardResult {
+    id: number;
+    title: string;
+    content: string;
+    writer_id: string;
+    written_time: string;
+    views: number;
+    likes: number;
+    category_order: number;
+}
+
+export interface BoardItem {
+    id: number;
+    writer: string;
+    title: string;
+    content: string;
+    written_time: string;
+    category_order: number;
+}
