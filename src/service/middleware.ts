2876427c047cdd004546c3bd20571b8d0ed2268e
Rufree <tksk2884@gmail.com>
2024-11-13 21:39:18 +0900

    ErrorMessage 비활성화

--- src/service/middleware.ts
+++ src/service/middleware.ts
@@ -1,5 +1,4 @@
 import { connectPool } from "./db";
-import { ERROR_DB_INVALID } from "../utils/errorMessage";
 import { getUserInfo } from "../utils/user";
 import jwt from "jsonwebtoken";
 import { UserInfo } from "../structure/type";
@@ -9,7 +8,7 @@ const JWT_SECRET: string = process.env.JWT_SECRET ?? "";
 export default async function middleware(req: any, res: any, next: () => void) {
     if (connectPool == null) {
         return res.status(500).json({
-            errorCode: ERROR_DB_INVALID,
+            errorCode: "",
             error: "DB connection failed",
         });
     }
